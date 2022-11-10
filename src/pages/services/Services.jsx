beaaf8bbf003c7decbaef16d2e7532d75a646434
Emdadul Islam <bekon1700@gmail.com>
2022-11-10 21:47:22 +0600

    some fix

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -9,7 +9,7 @@ const Services = () => {
   return (
     <div className='py-24'>
       <div className='flex flex-col gap-12'>
-        <p className='text-center font-semibold text-3xl'>Our Top Services</p>
+        <p className='text-center font-semibold text-3xl'>Our Services</p>
         <div className='w-11/12 lg:w-10/12 mx-auto grid grid-cols-1 lg:grid-cols-3 gap-6'>
           {
             services.map(el => <ServiceCard key={el._id} data={el} />)
